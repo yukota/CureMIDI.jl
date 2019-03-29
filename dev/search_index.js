3aed80bd42c60d913eb597c17184cbb93fdb10ee
autodocs <autodocs>
2019-03-29 10:16:47 +0000

    build based on 6b45b9e

---
+++ dev/search_index.js
@@ -0,0 +1,43 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Introduction",
+    "title": "Introduction",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#Introduction-1",
+    "page": "Introduction",
+    "title": "Introduction",
+    "category": "section",
+    "text": "CureMIDI.jl is a utility package for playing MIDI."
+},
+
+{
+    "location": "reference/#",
+    "page": "Reference",
+    "title": "Reference",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "reference/#CureMIDI.synth",
+    "page": "Reference",
+    "title": "CureMIDI.synth",
+    "category": "function",
+    "text": "Synthesize a block of 16 bit audio samples to audio buffers.\n\nArguments\n\nlen : Count of audio frames to synthesize\nbps : Beat per seconds. Beat means quater notes in this library.\n\n\n\n\n\n"
+},
+
+{
+    "location": "reference/#Reference-1",
+    "page": "Reference",
+    "title": "Reference",
+    "category": "section",
+    "text": "CurrentModule = CureMIDIsynth"
+},
+
+]}
